ec3f0528e5b7916262ed178c33f2ed2aaf6e71c7
Hassan11024 <hsnshariq@gmail.com>
2026-07-03 00:44:04 +0500

    Add main component to render MathsSolutionFinder

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MathsSolutionFinder from "./MathsSolutionFinder.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MathsSolutionFinder />
+  </React.StrictMode>
+);
